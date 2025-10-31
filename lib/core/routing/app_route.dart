a0c8241871098f4a34579632f69fe29aa6429fdc
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-31 21:41:37 +0300

    -refactor app_route to improve navigation and route management

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app_test/core/di/dependency_injection.dart';
 import 'package:movie_app_test/core/routing/routes.dart';
+import 'package:movie_app_test/features/home/presentation/cubit/home_cubit.dart';
 import 'package:movie_app_test/features/home/presentation/screen/movies_view.dart';
 
 class AppRouting {
@@ -7,7 +10,12 @@ class AppRouting {
   static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case Routes.moviesScreen:
-        return MaterialPageRoute(builder: (_) => const MoviesView());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => sl<HomeCubit>()..movieState(isRefrash: false),
+            child: const MoviesView(),
+          ),
+        );
 
       default:
         return defaultPage();
