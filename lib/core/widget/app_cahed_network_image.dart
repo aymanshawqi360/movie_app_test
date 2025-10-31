9a2f05cd4c19b728c72bd6ce4cb596a60d67ee95
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-31 21:36:00 +0300

    -create reusable core widgets

---
+++ lib/core/widget/app_cahed_network_image.dart
@@ -0,0 +1,41 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class AppCahedNetworkImage extends StatelessWidget {
+  final String image;
+  final double? width;
+  final double? height;
+  final Widget placeholder;
+  final BoxFit? fit;
+
+  const AppCahedNetworkImage({
+    super.key,
+
+    required this.image,
+    this.width,
+    this.fit,
+    this.height,
+    required this.placeholder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: image,
+      height: height,
+      width: width,
+      fit: fit ?? BoxFit.contain,
+      placeholder: (context, url) {
+        return placeholder;
+      },
+      errorWidget: (context, url, error) {
+        return const Icon(
+          Icons.image_not_supported_outlined,
+          color: Colors.white38,
+          size: 28,
+        );
+        // Icon(Icons.error, color: Colors.red);
+      },
+    );
+  }
+}
