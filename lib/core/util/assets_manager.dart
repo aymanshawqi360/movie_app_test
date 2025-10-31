41907fbe34e66fef5c0e6ced758d34505854b493
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-31 21:39:59 +0300

    -added assets && text manager to util folder

---
+++ lib/core/util/assets_manager.dart
@@ -0,0 +1,8 @@
+class AssetsManager {
+  AssetsManager._();
+  static const String _assets = 'assets/images/';
+  static const String _images = '';
+
+  //! Images
+  static const String imageNotFound = "${_assets}Image_not_found.jpg";
+}
