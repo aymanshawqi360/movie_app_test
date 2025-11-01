a1b44c4a5a5ecb1f3e0d0b1a093457aab92f86d9
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-01 10:24:26 +0300

    -added colors_manager to app

---
+++ lib/core/util/colors_manager.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class ColorsManager {
+  ColorsManager._();
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFBFBFC);
+  static Color gray = Color(0xFF7F7F7F);
+  static Color gray6 = Color(0xFF1C1C1E);
+  static const Color blue = Color(0xFF2986cc);
+
+  static const Color gruyDark = Color(0xFF68656E);
+  static const Color softGray = Color(0xFFA7A5AF);
+
+  static const Color purple = Color(0xFF452CE8);
+  static Color onyx = Color(0xFF454545);
+  static const Color lightGray = Color(0xFFEEEEEE);
+  static const Color orange = Color(0xFFfb6802);
+  static const Color turquoise = Color(0xFF07C4A5);
+}
